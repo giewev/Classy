c0aa47e64bdcd412a49935b3372fe35acaf36bb2
Ian Fade <giewev@gmail.com>
2016-11-01 18:29:29 -0400

    Add test for second perft board

--- src/Tests.cpp
+++ src/Tests.cpp
@@ -20,7 +20,21 @@ void startingPerft_test()
 
     double expectedPerfts[10] = { 1, 20, 400, 8902, 197281, 4865609, 119060324};
 
-    for(int i = 0; i < 6; i++)
+    for(int i = 0; i < 5; i++)
+    {
+        assert(testBoard.perft(i) == expectedPerfts[i]);
+    }
+}
+
+void kiwipetePerft_test()
+{
+    std::string kiwipeteFEN = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -";
+    Board testBoard = Board();
+    testBoard.loadFEN(kiwipeteFEN);
+
+    double expectedPerfts[10] = { 1, 48, 2039, 97862, 4085603, 193690690 };
+
+    for(int i = 0; i < 5; i++)
     {
         assert(testBoard.perft(i) == expectedPerfts[i]);
     }
@@ -67,4 +81,5 @@ void runAllTests()
 {
     loadStartingPosition_test();
     startingPerft_test();
+    kiwipetePerft_test();
 }
