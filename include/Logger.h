07c933ca7d4d853d27352fb4d82c460c1a172d7a
Ian Fade <giewev@gmail.com>
2017-02-26 00:33:03 -0500

    Change logger calling structure

--- include/Logger.h
+++ include/Logger.h
@@ -1,14 +1,19 @@
 #ifndef LOGGER_H
 #define LOGGER_H
+
+#include <string>
 #include "spdlog/spdlog.h"
 
 class Logger
 {
     public:
-        Logger();
+        void writeInfo(std::string message);
+        Logger mainLog();
     protected:
     private:
+    static Logger mainLogInstance;
     std::shared_ptr<spdlog::logger> log;
+    Logger();
 };
 
 #endif // LOGGER_H
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,12 +1,23 @@
+#include <string>
 #include "Logger.h"
 #include "spdlog/spdlog.h"
 
+Logger Logger::mainLogInstance;
+
 Logger::Logger()
 {
-    int logSizeMb = 5;
-    int mbSize = 1048576;
-    int fileCount = 3;
-    this->log = spdlog::rotating_logger_mt("MainLogger", "Classy.log", logSizeMb * mbSize, fileCount);
-    for (int i = 0; i < 10; ++i)
-            this->log->info("{} * {} equals {:>10}", i, i, i*i);
+    const int logSizeMb = 5;
+    const int mbSize = 1048576;
+    const int fileCount = 3;
+    this->log = spdlog::rotating_logger_mt("MainLogger", "Classy", logSizeMb * mbSize, fileCount);
+}
+
+Logger Logger::mainLog()
+{
+    return this->mainLogInstance;
+}
+
+void Logger::writeInfo(std::string message)
+{
+    this->log->info(message);
 }
